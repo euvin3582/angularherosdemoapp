1c1cb8385b240214d24e99977fee249841af2a5f
JEuvin <jose@klobot.ai>
2019-09-08 22:54:38 -0400

    Adding cart component and cart service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,30 +8,28 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent }
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent
-  ],
-  bootstrap: [ AppComponent ]
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+         { path: '', component: ProductListComponent },
+         { path: 'products/:productId', component: ProductDetailsComponent },
+         { path: 'cart', component: CartComponent }
+      ])
+   ],
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent,
+      CartComponent
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
